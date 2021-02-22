5aed114340eac4ce52a4251044ce6bce5485709f
vincentj2 <vincentj@naver.com>
2021-02-22 22:24:34 +0900

    AppConfig 파일을 통해 ServiceImpl에 DI(의존성주입), DIP,OCP 위반x

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,19 +1,26 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
     /*만약 FixDiscountPolicy() -> RateDiscountPolicy()로 변경하게 된다면
     OrderServiceImpl 자체의 코드 변경이 필요해지기 때문에 DIP, OCP 원칙을 위반하게 된다.
     그런경우 누군가 private final DiscountPolicy discountPolicy 같은 형태로 의존성 주입을 해줘야한다.
     -> 지금 코드는 기능을 확장해서 변경하면 클라이언트 코드에 영향을 준다
+
+    변경후
+    생성자를 사용해 AppConfig에서 구현체를 직접 관리해주고 이곳에 주입해준다.
+    이것을 의존성 주입 DI라고 한다
     */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
