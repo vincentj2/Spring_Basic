f807fce6f92cb4831ba59e4ef8038184eae13c84
vincentj2 <vincentj@naver.com>
2021-08-18 00:57:05 +0900

    싱글톤 컨테이너 마무리

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     //컨커런트 해시맵 사용시 동시성 이슈 해결 가능 -> 알아보기
