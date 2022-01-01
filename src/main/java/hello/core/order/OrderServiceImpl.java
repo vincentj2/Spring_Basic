f7baaa9fc514f706a0207221de76bfd8d666c9e7
Sangwon <vincentj@naver.com>
2022-01-01 23:23:05 +0900

    fix

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -19,6 +19,8 @@ public class OrderServiceImpl implements OrderService {
     }
     /*만약 FixDiscountPolicy() -> RateDiscountPolicy()로 변경하게 된다면
     OrderServiceImpl 자체의 코드 변경이 필요해지기 때문에 DIP, OCP 원칙을 위반하게 된다.
+    -> 인터페이스에만 의존하도록 설계를 변경해야한다.
+
     그런경우 누군가 private final DiscountPolicy discountPolicy 같은 형태로 의존성 주입을 해줘야한다.
     -> 지금 코드는 기능을 확장해서 변경하면 클라이언트 코드에 영향을 준다
 
